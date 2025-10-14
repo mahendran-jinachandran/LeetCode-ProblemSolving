6d578eb2fb806c700e6d327dadc6be841858efb5
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2025-10-14 10:18:48 +0100

    [268] [Time Beats: 100.00%] [Memory Beats: 22.44%] - LeetPush

---
+++ Missing-Number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int count[] = new int[nums.length+1]; 
+        for(int i=0;i<nums.length;i++){
+            count[nums[i]]++;
+        }   
+        for(int i=0;i<=nums.length;i++){
+            if(count[i]==0) return i;
+        }
+        
+        return -1;
+    }
+}
