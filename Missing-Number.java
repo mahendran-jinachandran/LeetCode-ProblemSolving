d9dec50c0e46e1c722e5d042d337fb87d1a6a498
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2025-10-14 10:25:09 +0100

    [268] [Time Beats: 100.00%] [Memory Beats: 86.20%] - LeetPush

--- Missing-Number.java
+++ Missing-Number.java
@@ -1,13 +1,13 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int count[] = new int[nums.length+1]; 
-        for(int i=0;i<nums.length;i++){
-            count[nums[i]]++;
-        }   
-        for(int i=0;i<=nums.length;i++){
-            if(count[i]==0) return i;
-        }
         
-        return -1;
+        int n = nums.length;
+        int sum = 0;
+        for(int i=0; i < n; i++) {
+            sum += nums[i];
+        }
+
+        int totalSum = (n * (n + 1)) / 2;
+        return totalSum - sum;
     }
 }
