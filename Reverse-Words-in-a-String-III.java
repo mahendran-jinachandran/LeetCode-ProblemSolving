b9e6b868d76c43d696b0f2a6e0a49cd5c52369ab
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2025-10-18 22:13:54 +0100

    [557] [Time Beats: 53.13%] [Memory Beats: 95.55%] - LeetPush

---
+++ Reverse-Words-in-a-String-III.java
@@ -0,0 +1,35 @@
+class Solution {
+
+
+    public String reverseWords(String s) {
+        
+        char[] newWord = s.toCharArray();
+        int i = 0;
+        int j = 0;
+        while(i < s.length()) {
+            if(j < s.length() && (newWord[j] == ' ' || j == s.length() - 1)) {
+                
+                int z = 0;
+                if(newWord[j] == ' ') {
+                    z = j - 1;
+                } else {
+                    z = j;
+                }
+
+                while(i < z) {
+                    char temp = newWord[i];
+                    newWord[i] = newWord[z];
+                    newWord[z] = temp;
+                    i++;
+                    z--;
+                }
+
+                i = j + 1;
+            }
+
+            j++;
+        }
+
+        return new String(newWord);
+    }
+}
