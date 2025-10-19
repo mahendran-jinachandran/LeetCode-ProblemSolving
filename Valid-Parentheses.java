35278f06fea7e7f112743e17e69ed04decd3c46c
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2025-10-19 10:39:56 +0100

    [20] [Time Beats: 97.33%] [Memory Beats: 85.88%] - LeetPush

--- Valid-Parentheses.java
+++ Valid-Parentheses.java
@@ -5,8 +5,8 @@ class Solution {
     public boolean isValid(String s) {
         Character[] parantheses = new Character[s.length()];
         int stackIndex = 0;
-
-        for(int i=0; i<s.length(); i++) {
+        int n = s.length();
+        for(int i=0; i<n; i++) {
             if (!(stackIndex==0) && ((parantheses[stackIndex - 1] == '(' && s.charAt(i) == ')') || 
                (parantheses[stackIndex - 1] == '{' && s.charAt(i) == '}') ||
                (parantheses[stackIndex - 1] == '[' && s.charAt(i) == ']')
