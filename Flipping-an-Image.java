67377a3ad2e94b4d9132eca91a90465a0775232c
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2025-10-19 10:46:16 +0100

    [832] [Time Beats: 35.76%] [Memory Beats: 14.38%] - LeetPush

--- Flipping-an-Image.java
+++ Flipping-an-Image.java
@@ -5,7 +5,8 @@ class Solution {
     }
     public int[][] flipAndInvertImage(int[][] image) {
         
-        for(int i = 0; i < image.length; i++) {
+        int n = image.length;
+        for(int i = 0; i < n; i++) {
 
             int z = image[i].length - 1;
             int j = 0;
