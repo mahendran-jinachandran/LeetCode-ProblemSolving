bd93579eac18d25bf778cbf1335b9e9ed7a47112
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2025-10-28 10:26:59 +0000

    [66] [Time Beats: 100.00%] [Memory Beats: 9.78%] - LeetPush

--- Plus-One.java
+++ Plus-One.java
@@ -1,33 +1,18 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        
-        int n = digits.length - 1;
-        int carry = 0;
-        ArrayList<Integer> newArray = new ArrayList<Integer>();
+        int n = digits.length;
 
-        for(int i = n; i >= 0; i--) {
-            
-            int sum = 0;
-            if(i == n) {
-                sum = digits[i] + 1;
-            } else {
-                sum = digits[i] + carry;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
             }
-            
-            newArray.add(sum % 10);      
-            carry = (sum >= 10) ? 1 : 0; 
+
+            digits[i] = 0;
         }
 
-        if(carry == 1) {
-            newArray.add(1);
-        }
-
-        Collections.reverse(newArray);
-
-        int[] ans = new int[newArray.size()];
-        for (int i = 0; i < newArray.size(); i++) {
-            ans[i] = newArray.get(i);
-        }
-        return ans;
+        int[] newDigits = new int[n + 1];
+        newDigits[0] = 1;
+        return newDigits;
     }
 }
