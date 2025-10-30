05c9b20092a0c08725611054cb9d54c813b47ec8
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2025-10-30 09:00:46 +0000

    [724] [Time Beats: 91.86%] [Memory Beats: 66.23%] - LeetPush

---
+++ Find-Pivot-Index.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int n = nums.length;
+        int total = 0;
+        int prefSum = 0;
+        for(int i = 0; i < n; i++) {
+            total += nums[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            int suffSum = total - prefSum - nums[i];
+            if (prefSum == suffSum) {
+                return i;
+            }
+            prefSum += nums[i];
+        }
+
+        return -1;
+    }
+}
