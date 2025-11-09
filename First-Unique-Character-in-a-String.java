64d80084fddfe8e5b4bbcb8a12f52a06976eb04d
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2025-11-09 00:54:15 +0000

    [387] [Time Beats: 95.51%] [Memory Beats: 10.67%] - LeetPush

--- First-Unique-Character-in-a-String.java
+++ First-Unique-Character-in-a-String.java
@@ -1,27 +1,19 @@
 class Solution {
     public int firstUniqChar(String s) {
-            HashMap<Character, Integer> characters = new HashMap<Character, Integer>();
-            int n = s.length();
+        int[] freq = new int[26];
+        int n = s.length();
+        
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
 
-            for(int i = 0; i < n; i++) {
-
-                char c = s.charAt(i);
-                if(characters.containsKey(c)) {
-                    
-                    characters.put(c, characters.get(c) + 1);
-
-                } else {
-                    characters.put(c, 1);
-                }
-            } 
-
-            for(int i = 0; i < n; i++) {
-
-                if(characters.get(s.charAt(i)) == 1) {
-                    return i;
-                }
+        // Find the first character with frequency 1
+        for (int i = 0 ; i < n ;i++) {
+            if (freq[s.charAt(i) - 'a'] == 1) {
+                return i;
             }
-
-            return -1;
+        }
+        return -1;
+        
     }
 }
