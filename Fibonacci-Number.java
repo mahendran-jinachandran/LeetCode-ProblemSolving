ab8688501d447fdcdd927c990707633bc80ba351
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2025-11-16 11:19:26 +0000

    [509] [Time Beats: 100.00%] [Memory Beats: 10.43%] - LeetPush

--- Fibonacci-Number.java
+++ Fibonacci-Number.java
@@ -1,15 +1,15 @@
 class Solution {
     public int fib(int n) {
+        int n1 = 0, n2 = 1;
 
-        if(n == 0) {
-            return 0;
+        for (int i = 0; i < n; i++) {
+ 
+            // Swap
+            int n3 = n2 + n1;
+            n1 = n2;
+            n2 = n3;
         }
 
-        if(n == 1) {
-            return 1;
-        }
-
-        return fib(n - 1) + fib(n-2);
-        
+        return n1;
     }
 }
