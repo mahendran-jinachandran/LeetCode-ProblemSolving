8bbbf34c255c2c2b4921690579aafa2848b6d2d2
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2025-12-16 00:34:38 +0000

    [94] [Time Beats: 100.00%] [Memory Beats: 30.79%] - LeetPush

--- Binary-Tree-Inorder-Traversal.java
+++ Binary-Tree-Inorder-Traversal.java
@@ -1,31 +1,30 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-
-    List<Integer> values = new ArrayList<>();
-
-    public List<Integer> inorderTraversal(TreeNode root) {
-        if (root == null) {
-            return values;
-        }
-
-        inorderTraversal(root.left);        
-        values.add(root.val);             
-        inorderTraversal(root.right); 
-
-        return values;
-    }
-}
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17
+18    List<Integer> ans = new LinkedList<Integer>();
+19    public List<Integer> inorderTraversal(TreeNode root) {
+20
+21        if(root == null) {
+22            return new LinkedList<Integer>();
+23        }
+24
+25        inorderTraversal(root.left);
+26        ans.add(root.val);
+27        inorderTraversal(root.right);
+28        return ans;
+29    }
+30}
