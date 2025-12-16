ccaed4c364e7c41c09284bb844d6f36c45e9631e
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2025-12-16 00:41:03 +0000

    [100] [Time Beats: 100.00%] [Memory Beats: 15.80%] - LeetPush

--- Same-Tree.java
+++ Same-Tree.java
@@ -1,45 +1,30 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-
-        if ((p != null && q == null) || (p == null && q != null)) {
-            return false;
-        }
-
-        if(p == null || q == null) {
-            return true;
-        }
-
-        if (p.val != q.val) {
-            return false;
-        }
-
-        boolean isNotSameLeft = isSameTree(p.left,q.left);
-        if(!isNotSameLeft) {
-            return false;
-        }
-
-        boolean isNotSameRight = isSameTree(p.right,q.right);
-        if(!isNotSameRight) {
-            return false;
-        }
-
-        return true;
-
-    }
-}
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public boolean isSameTree(TreeNode p, TreeNode q) {
+18
+19        if(p == null && q == null) {
+20            return true;
+21        }
+22        
+23        if(p == null || q == null || p.val != q.val) {
+24            return false;
+25        }
+26
+27
+28        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+29    }
+30}
