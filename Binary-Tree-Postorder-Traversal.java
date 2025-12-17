193e2cfd263cadddcbd65b2f36ed3969c06c23d3
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2025-12-17 09:09:39 +0000

    [145] [Time Beats: 100.00%] [Memory Beats: 71.41%] - LeetPush

--- Binary-Tree-Postorder-Traversal.java
+++ Binary-Tree-Postorder-Traversal.java
@@ -1,30 +1,32 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-
-    List<Integer> values = new ArrayList<Integer>();
-    public List<Integer> postorderTraversal(TreeNode root) {
-        
-        if(root == null) {
-            return values;
-        }
-
-        postorderTraversal(root.left);
-        postorderTraversal(root.right);
-        values.add(root.val);
-        return values;
-    }
-}
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17
+18    List<Integer> traversal = new LinkedList<>();
+19    public List<Integer> postorderTraversal(TreeNode root) {
+20
+21        if(root == null) {
+22            return new ArrayList<>();
+23        }
+24
+25
+26        postorderTraversal(root.left);
+27        postorderTraversal(root.right);
+28        traversal.add(root.val);
+29        return traversal;
+30        
+31    }
+32}
