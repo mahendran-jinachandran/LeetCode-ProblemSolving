30297c1ec875ff835b49761af19888a0d1145ed4
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2025-12-17 12:44:52 +0000

    [104] [Time Beats: 100.00%] [Memory Beats: 28.06%] - LeetPush

--- Maximum-Depth-of-Binary-Tree.java
+++ Maximum-Depth-of-Binary-Tree.java
@@ -1,27 +1,28 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public int maxDepth(TreeNode root) {
-        
-        if(root == null) {
-            return 0;
-        }
-
-        int lHeight = maxDepth(root.left);
-        int rHeight = maxDepth(root.right);
-        return Math.max(lHeight, rHeight) + 1;
-    }
-}
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public int maxDepth(TreeNode root) {
+18
+19        if(root == null) {
+20            return 0;
+21        }
+22
+23        int left = 1 + maxDepth(root.left);
+24        int right = 1 + maxDepth(root.right);
+25        return Math.max(left, right);
+26        
+27    }
+28}
