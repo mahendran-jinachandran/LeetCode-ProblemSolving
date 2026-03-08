e6c87f0da370be939f1881cc04618153ace587c5
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2026-03-08 11:25:21 +0000

    [114] [Time Beats: 100.00%] [Memory Beats: 92.38%] - LeetPush

---
+++ Flatten-Binary-Tree-to-Linked-List.java
@@ -0,0 +1,30 @@
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17
+18    TreeNode prev = null;
+19    public void flatten(TreeNode root) {
+20        if(root == null) {
+21            return;
+22        }  
+23
+24        flatten(root.right);
+25        flatten(root.left);
+26        root.right = prev;
+27        root.left = null;
+28        prev = root;
+29    }
+30}
