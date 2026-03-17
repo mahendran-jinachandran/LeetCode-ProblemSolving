1977ce3378e3e3f6b28494313e03f25bb5a23167
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2026-03-17 07:57:41 +0000

    [1512] [Time Beats: 100.00%] [Memory Beats: 38.78%] - LeetPush

--- Number-of-Good-Pairs.java
+++ Number-of-Good-Pairs.java
@@ -1,15 +1,12 @@
 1class Solution {
 2    public int numIdenticalPairs(int[] nums) {
-3        Map<Integer, Integer> pairs = new HashMap<>();
-4        int count = 0;
-5
-6        for (int i = 0; i < nums.length; i++) {
-7            if (pairs.containsKey(nums[i])) {
-8                count += pairs.get(nums[i]);
-9            }
-10            pairs.put(nums[i], pairs.getOrDefault(nums[i], 0) + 1);
-11        }
-12
-13        return count;        
-14    }
-15}
+3       int freq[]=new int[101];
+4        int pair=0;
+5        for(int num: nums){
+6          pair +=freq[num];
+7             
+8            freq[num]++;
+9        }
+10        return pair;
+11    }
+12}
