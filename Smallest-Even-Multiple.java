f16e49dbb0208eb45fe5c31481f5e22c91460894
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2026-03-22 07:44:19 +0000

    [2413] [Time Beats: 100.00%] [Memory Beats: 25.54%] - LeetPush

--- Smallest-Even-Multiple.java
+++ Smallest-Even-Multiple.java
@@ -1,17 +1,10 @@
 1class Solution {
 2    public int smallestEvenMultiple(int n) {
 3        
-4        int number = 0;
-5        int mult = n;
-6        while(true) {
-7            if(mult % n == 0 && mult % 2 == 0) {
-8                number = mult;
-9                break;
-10            }
-11
-12            mult *= 2;
-13        }
-14
-15        return number;
-16    }
-17}
+4        if(n%2==0){
+5            return n;
+6        } else{
+7            return 2*n;
+8        }
+9    }
+10}
