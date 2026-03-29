27900162b27f4afe873d407aaf287d4b696c2caf
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2026-03-29 23:01:39 +0100

    [912] [Time Beats: 71.94%] [Memory Beats: 85.11%] - LeetPush

--- Sort-an-Array.java
+++ Sort-an-Array.java
@@ -1,41 +1,43 @@
 1class Solution {
 2
 3    static void merge(int a[], int b[], int lb, int mid, int ub) {
-4        int i = lb, j = mid + 1, k = lb;
-5
-6        while (i <= mid && j <= ub) {
-7            if (a[i] < a[j]) {
-8                b[k++] = a[i++];
-9            } else {
-10                b[k++] = a[j++];
-11            }
-12        }
-13
-14        while (i <= mid) {
-15            b[k++] = a[i++];
-16        }
-17
-18        while (j <= ub) {
-19            b[k++] = a[j++];
-20        }
-21
-22        for (k = lb; k <= ub; k++) {
-23            a[k] = b[k];
-24        }
-25    }
-26
-27    static void mergeSort(int a[], int b[], int lb, int ub) {
-28        if (lb < ub) {
-29            int mid = (lb + ub) / 2;
-30            mergeSort(a, b, lb, mid);
-31            mergeSort(a, b, mid + 1, ub);
-32            merge(a, b, lb, mid, ub);
-33        }
-34    }
-35
-36    public int[] sortArray(int[] nums) {
-37        int[] b = new int[nums.length];
-38        mergeSort(nums, b, 0, nums.length - 1);
-39        return nums;
-40    }
-41}
+4        int i = lb;
+5        int j = mid + 1;
+6        int k = lb;
+7
+8        while (i <= mid && j <= ub) {
+9            if (a[i] < a[j]) {
+10                b[k++] = a[i++];
+11            } else {
+12                b[k++] = a[j++];
+13            }
+14        }
+15
+16        while (i <= mid) {
+17            b[k++] = a[i++];
+18        }
+19
+20        while (j <= ub) {
+21            b[k++] = a[j++];
+22        }
+23
+24        for (k = lb; k <= ub; k++) {
+25            a[k] = b[k];
+26        }
+27    }
+28
+29    static void mergeSort(int a[], int b[], int lb, int ub) {
+30        if (lb < ub) {
+31            int mid = (lb + ub) / 2;
+32            mergeSort(a, b, lb, mid);
+33            mergeSort(a, b, mid + 1, ub);
+34            merge(a, b, lb, mid, ub);
+35        }
+36    }
+37
+38    public int[] sortArray(int[] nums) {
+39        int[] b = new int[nums.length];
+40        mergeSort(nums, b, 0, nums.length - 1);
+41        return nums;
+42    }
+43}
