42f6292ae9370ce7e26b57de825a2e7441cbd858
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2026-04-01 11:04:17 +0100

    [75] [Time Beats: 100.00%] [Memory Beats: 52.79%] - LeetPush

--- Sort-Colors.java
+++ Sort-Colors.java
@@ -1,25 +1,41 @@
 1class Solution {
 2    public void sortColors(int[] nums) {
 3        
-4        int low = 0;
-5        int mid = 0;
-6        int high = nums.length - 1;
+4        int redCount = 0;
+5        int whiteCount = 0;
+6        int blueCount = 0;
 7
-8        while(mid <= high) {
-9            if(nums[mid] == 0) {
-10                int temp = nums[low];
-11                nums[low] = nums[mid];
-12                nums[mid] = temp;
-13                low++;
-14                mid++;
-15            } else if(nums[mid] == 1) {
-16                mid++;
-17            } else {
-18                int temp = nums[high];
-19                nums[high] = nums[mid];
-20                nums[mid] = temp;
-21                high--;
-22            }
-23        }
-24    }
-25}
+8        for(int num: nums) {
+9            if(num == 0) {
+10                redCount++;
+11            } else if(num == 1) {
+12                whiteCount++;
+13            } else {
+14                blueCount++;
+15            }
+16        }
+17
+18
+19
+20        int i = 0;
+21        int k = 0;
+22        while(i < redCount) {
+23            nums[k++] = 0; 
+24            i++;
+25        }
+26
+27        i = 0;
+28        while(i < whiteCount) {
+29            nums[k++] = 1; 
+30            i++;
+31        }
+32
+33        i = 0;
+34        while(i < blueCount) {
+35            nums[k++] = 2; 
+36            i++;
+37        }
+38
+39
+40    }
+41}
