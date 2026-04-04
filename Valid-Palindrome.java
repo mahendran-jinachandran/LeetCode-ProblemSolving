40e94c9ac54a6e5c8c952052c4eff3ae4ad03f6e
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2026-04-04 10:24:57 +0100

    [125] [Time Beats: 99.41%] [Memory Beats: 79.91%] - LeetPush

--- Valid-Palindrome.java
+++ Valid-Palindrome.java
@@ -1,40 +1,40 @@
-class Solution {
-
-    boolean isAlphaNumberic(char c) {
-        return (c >= 65 && c <= 90) ||
-           (c >= 97 && c <= 122) ||
-           (c >= 48 && c <= 57);
-    }
-
-    public boolean isPalindrome(String s) {
-
-        s = s.toLowerCase();
-        if(s.isEmpty()) { return true; }
-        
-        int i = 0;
-        int j = s.length() - 1;
-
-        while(i <= j) {
-            char firstLetter = s.charAt(i);
-            if(!isAlphaNumberic(firstLetter)) {
-                i++;
-                continue;
-            }
-
-            char lastLetter = s.charAt(j);
-            if(!isAlphaNumberic(lastLetter)) {
-                j--;
-                continue;
-            }
-
-            if(firstLetter != lastLetter) {
-                return false;
-            }
-
-            i++;
-            j--;
-        }
-
-        return true;
-    }
-}
+1class Solution {
+2
+3    boolean isAlphaNumberic(char c) {
+4        return (c >= 65 && c <= 90) ||
+5           (c >= 97 && c <= 122) ||
+6           (c >= 48 && c <= 57);
+7    }
+8
+9    public boolean isPalindrome(String s) {
+10
+11        s = s.toLowerCase();
+12        if(s.isEmpty()) { return true; }
+13        
+14        int i = 0;
+15        int j = s.length() - 1;
+16
+17        while(i <= j) {
+18            char firstLetter = s.charAt(i);
+19            if(!isAlphaNumberic(firstLetter)) {
+20                i++;
+21                continue;
+22            }
+23
+24            char lastLetter = s.charAt(j);
+25            if(!isAlphaNumberic(lastLetter)) {
+26                j--;
+27                continue;
+28            }
+29
+30            if(firstLetter != lastLetter) {
+31                return false;
+32            }
+33
+34            i++;
+35            j--;
+36        }
+37
+38        return true;
+39    }
+40}
