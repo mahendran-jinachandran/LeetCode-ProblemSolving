0afdc195ed23ae7e88cbaa0e7402405fb3b92e3b
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2026-04-07 18:06:04 +0100

    [26] [Time Beats: 77.42%] [Memory Beats: 86.06%] - LeetPush

--- Remove-Duplicates-from-Sorted-Array.java
+++ Remove-Duplicates-from-Sorted-Array.java
@@ -1,12 +1,20 @@
-class Solution {
-    public int removeDuplicates(int[] nums) {
-        int idx = 1;
-        for(int i = 1; i<nums.length; i++) {
-            if(nums[i] != nums[i-1]) {
-                nums[idx++] = nums[i];
-            }
-        }
-
-        return idx;
-    }
-}
+1class Solution {
+2    public int removeDuplicates(int[] nums) {
+3        int n = nums.length;
+4
+5        if(n == 0) {
+6            return 0;
+7        }
+8        
+9        int end = 0;
+10        for(int i = 1; i < n; i++) {
+11            if(nums[i - 1] != nums[i]) {
+12                end++;
+13                nums[end] = nums[i];
+14            }
+15        }
+16
+17        return end + 1;
+18        
+19    }
+20}
