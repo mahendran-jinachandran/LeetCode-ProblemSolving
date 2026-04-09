623fc9f2be88f12951d96826d803656d3b038d83
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2026-04-09 07:41:00 +0100

    [88] [Time Beats: 100.00%] [Memory Beats: 50.63%] - LeetPush

--- Merge-Sorted-Array.java
+++ Merge-Sorted-Array.java
@@ -1,22 +1,22 @@
-class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        
-        int p1 = m - 1, p2 = n - 1, p = m + n - 1;
-
-        while(p1 >= 0 && p2 >= 0) {
-            if(nums1[p1] > nums2[p2]) {
-                nums1[p] = nums1[p1];
-                p1--;
-            } else {
-                nums1[p] = nums2[p2];
-                p2--;
-            }
-
-            p--;
-        }
-
-        while (p2 >= 0) {
-            nums1[p--] = nums2[p2--];
-        }
-    }
-}
+1class Solution {
+2    public void merge(int[] nums1, int m, int[] nums2, int n) {
+3        
+4        int p1 = m - 1, p2 = n - 1, p = m + n - 1;
+5
+6        while(p1 >= 0 && p2 >= 0) {
+7            if(nums1[p1] > nums2[p2]) {
+8                nums1[p] = nums1[p1];
+9                p1--;
+10            } else {
+11                nums1[p] = nums2[p2];
+12                p2--;
+13            }
+14
+15            p--;
+16        }
+17
+18        while (p2 >= 0) {
+19            nums1[p--] = nums2[p2--];
+20        }
+21    }
+22}
