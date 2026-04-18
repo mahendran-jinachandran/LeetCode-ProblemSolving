5ba2099242fe9e55fdc2fb6314dfb779626ba866
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2026-04-18 07:42:42 +0100

    [682] [Time Beats: 82.71%] [Memory Beats: 69.37%] - LeetPush

--- Baseball-Game.java
+++ Baseball-Game.java
@@ -12,32 +12,22 @@
 12                int finalValue = firstValue + secondValue;
 13                op.push(Integer.toString(secondValue));
 14                op.push(Integer.toString(firstValue));
-15                op.push(Integer.toString(finalValue));
-16                // System.out.println("+");
-17                // op.forEach(System.out::println);
-18                
-19            } else if(o.equals("D")) {
-20                int doubleValue = Integer.parseInt(op.peek()) * 2;
-21                op.push(Integer.toString(doubleValue));
-22                // System.out.println("D");
-23                // op.forEach(System.out::println);
-24            } else if(o.equals("C")) {
-25                op.pop();
-26                // System.out.println("C");
-27                // op.forEach(System.out::println);
-28            } else {
-29                op.push(o);
-30                // System.out.println("FINAL");
-31                // op.forEach(System.out::println);
-32            }
-33        }
-34
-35        int sum = 0;
-36        while(!op.isEmpty()) {
-37            // System.out.println(op.pop());
-38             sum += Integer.parseInt(op.pop());
-39        }
-40
-41        return sum;
-42    }
-43}
+15                op.push(Integer.toString(finalValue));       
+16            } else if(o.equals("D")) {
+17                int doubleValue = Integer.parseInt(op.peek()) * 2;
+18                op.push(Integer.toString(doubleValue));
+19            } else if(o.equals("C")) {
+20                op.pop();
+21            } else {
+22                op.push(o);
+23            }
+24        }
+25
+26        int sum = 0;
+27        while(!op.isEmpty()) {
+28             sum += Integer.parseInt(op.pop());
+29        }
+30
+31        return sum;
+32    }
+33}
