d4931dd26e99b147991ba5ea1c2676fe09a39213
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2026-04-20 11:05:44 +0100

    [20] [Time Beats: 97.95%] [Memory Beats: 99.54%] - LeetPush

--- Valid-Parentheses.java
+++ Valid-Parentheses.java
@@ -1,26 +1,26 @@
-class Solution {
-
-    
-
-    public boolean isValid(String s) {
-        Character[] parantheses = new Character[s.length()];
-        int stackIndex = 0;
-        int n = s.length();
-        for(int i=0; i<n; i++) {
-            if (!(stackIndex==0) && ((parantheses[stackIndex - 1] == '(' && s.charAt(i) == ')') || 
-               (parantheses[stackIndex - 1] == '{' && s.charAt(i) == '}') ||
-               (parantheses[stackIndex - 1] == '[' && s.charAt(i) == ']')
-            )) {
-
-                parantheses[stackIndex] = '$';
-                stackIndex--;
-
-            } else {
-               parantheses[stackIndex] = s.charAt(i);
-               stackIndex++;
-            }
-        }
-
-        return stackIndex==0;
-    }
-}
+1class Solution {
+2
+3    
+4
+5    public boolean isValid(String s) {
+6        Character[] parantheses = new Character[s.length()];
+7        int stackIndex = 0;
+8        int n = s.length();
+9        for(int i=0; i<n; i++) {
+10            if (!(stackIndex==0) && ((parantheses[stackIndex - 1] == '(' && s.charAt(i) == ')') || 
+11               (parantheses[stackIndex - 1] == '{' && s.charAt(i) == '}') ||
+12               (parantheses[stackIndex - 1] == '[' && s.charAt(i) == ']')
+13            )) {
+14
+15                parantheses[stackIndex] = '$';
+16                stackIndex--;
+17
+18            } else {
+19               parantheses[stackIndex] = s.charAt(i);
+20               stackIndex++;
+21            }
+22        }
+23
+24        return stackIndex==0;
+25    }
+26}
