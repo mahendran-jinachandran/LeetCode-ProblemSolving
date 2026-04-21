989940ca13bce49a98e8f461fb7e75ccb4c86931
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2026-04-21 07:35:29 +0100

    [141] [Time Beats: 100.00%] [Memory Beats: 36.02%] - LeetPush

--- Linked-List-Cycle.java
+++ Linked-List-Cycle.java
@@ -1,28 +1,28 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
- * }
- */
-public class Solution {
-    public boolean hasCycle(ListNode head) {
-        
-        ListNode fast = head, slow = head;
-
-        while(slow != null && fast != null && fast.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
-
-            if(slow == fast) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-}
+1/**
+2 * Definition for singly-linked list.
+3 * class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode(int x) {
+7 *         val = x;
+8 *         next = null;
+9 *     }
+10 * }
+11 */
+12public class Solution {
+13    public boolean hasCycle(ListNode head) {
+14        
+15        ListNode fast = head, slow = head;
+16
+17        while(slow != null && fast != null && fast.next != null) {
+18            fast = fast.next.next;
+19            slow = slow.next;
+20
+21            if(slow == fast) {
+22                return true;
+23            }
+24        }
+25
+26        return false;
+27    }
+28}
