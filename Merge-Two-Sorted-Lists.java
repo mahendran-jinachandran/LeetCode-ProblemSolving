c940bacf08bedbc805393c9e6b3cbdc814a15fe6
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2026-04-22 07:22:47 +0100

    [21] [Time Beats: 100.00%] [Memory Beats: 22.85%] - LeetPush

--- Merge-Two-Sorted-Lists.java
+++ Merge-Two-Sorted-Lists.java
@@ -1,38 +1,38 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        
-        ListNode combinedList = new ListNode(-1,null);
-        ListNode head = combinedList;
-
-        while(list1 != null && list2 != null) {
-
-            if(list1.val <= list2.val) {
-                combinedList.next = list1;
-                list1 = list1.next;
-            } else {
-                combinedList.next = list2;
-                list2 = list2.next;
-            }
-
-            combinedList = combinedList.next;
-        }
-
-        if(list1 != null) {
-            combinedList.next = list1;
-        } else {
-            combinedList.next = list2;
-        }
-
-        return head.next;
-    }
-}
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+13        
+14        ListNode combinedList = new ListNode(-1,null);
+15        ListNode head = combinedList;
+16
+17        while(list1 != null && list2 != null) {
+18
+19            if(list1.val <= list2.val) {
+20                combinedList.next = list1;
+21                list1 = list1.next;
+22            } else {
+23                combinedList.next = list2;
+24                list2 = list2.next;
+25            }
+26
+27            combinedList = combinedList.next;
+28        }
+29
+30        if(list1 != null) {
+31            combinedList.next = list1;
+32        } else {
+33            combinedList.next = list2;
+34        }
+35
+36        return head.next;
+37    }
+38}
