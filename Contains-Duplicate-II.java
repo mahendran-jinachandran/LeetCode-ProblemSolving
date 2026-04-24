2e5cf160a37de01b47d7aa1e4ecf4612f0978d44
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2026-04-24 07:36:55 +0100

    [219] [Time Beats: 90.85%] [Memory Beats: 10.15%] - LeetPush

--- Contains-Duplicate-II.java
+++ Contains-Duplicate-II.java
@@ -1,22 +1,22 @@
-class Solution {
-    public boolean containsNearbyDuplicate(int[] nums, int k) {
-        HashMap<Integer, Integer> numbers = new HashMap<Integer, Integer>();
-
-        int n = nums.length;
-        boolean containsDuplicate = false;
-        for(int i = 0; i <n; i++ ) {
-
-            if(numbers.containsKey(nums[i])) {
-                if(Math.abs(numbers.get(nums[i]) - i) <= k) {
-                    return true;
-                } else {
-                    numbers.put(nums[i], i);
-                }
-            } else {
-                numbers.put(nums[i], i);
-            }
-        }
-
-        return false;
-    }
-}
+1class Solution {
+2    public boolean containsNearbyDuplicate(int[] nums, int k) {
+3        HashMap<Integer, Integer> numbers = new HashMap<Integer, Integer>();
+4
+5        int n = nums.length;
+6        boolean containsDuplicate = false;
+7        for(int i = 0; i <n; i++ ) {
+8
+9            if(numbers.containsKey(nums[i])) {
+10                if(Math.abs(numbers.get(nums[i]) - i) <= k) {
+11                    return true;
+12                } else {
+13                    numbers.put(nums[i], i);
+14                }
+15            } else {
+16                numbers.put(nums[i], i);
+17            }
+18        }
+19
+20        return false;
+21    }
+22}
