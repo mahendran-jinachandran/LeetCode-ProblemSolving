7ca77fc427d28b36de0c8d27faf5f471af040618
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2026-04-25 08:06:36 +0100

    [704] [Time Beats: 100.00%] [Memory Beats: 70.82%] - LeetPush

--- Binary-Search.java
+++ Binary-Search.java
@@ -1,23 +1,23 @@
-class Solution {
-    public int search(int[] nums, int target) {
-        
-        int low = 0;
-        int high = nums.length - 1;
-
-        while(low <= high) {
-            int mid = (low + high) / 2;
-
-            if(nums[mid] == target) {
-                return mid;
-            }
-
-            if(target < nums[mid]) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-
-        return -1;
-    }
-}
+1class Solution {
+2    public int search(int[] nums, int target) {
+3        
+4        int low = 0;
+5        int high = nums.length - 1;
+6
+7        while(low <= high) {
+8            int mid = (low + high) / 2;
+9
+10            if(nums[mid] == target) {
+11                return mid;
+12            }
+13
+14            if(target < nums[mid]) {
+15                high = mid - 1;
+16            } else {
+17                low = mid + 1;
+18            }
+19        }
+20
+21        return -1;
+22    }
+23}
