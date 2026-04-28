84b67ff4439bf9a1e3fe0609a588a93487514883
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2026-04-28 09:39:12 +0100

    [69] [Time Beats: 97.84%] [Memory Beats: 42.27%] - LeetPush

--- Sqrt(x).java
+++ Sqrt(x).java
@@ -1,26 +1,26 @@
-class Solution {
-    public int mySqrt(int x) {
-
-        if (x == 0 || x == 1)
-            return x;
-
-        int start = 1;
-        int end = x;
-        int mid = -1;
-
-
-        while (start <= end) {
-      
-            mid = start + (end - start) / 2;
-
-            if ((long) mid * mid > (long) x)
-                end = mid - 1;
-            else if (mid * mid == x)
-                return mid;
-            else
-                start = mid + 1;
-        }
-        
-        return Math.round(end);
-    }
-}
+1class Solution {
+2    public int mySqrt(int x) {
+3
+4        if (x == 0 || x == 1)
+5            return x;
+6
+7        int start = 1;
+8        int end = x;
+9        int mid = -1;
+10
+11
+12        while (start <= end) {
+13      
+14            mid = start + (end - start) / 2;
+15
+16            if ((long) mid * mid > (long) x)
+17                end = mid - 1;
+18            else if (mid * mid == x)
+19                return mid;
+20            else
+21                start = mid + 1;
+22        }
+23        
+24        return Math.round(end);
+25    }
+26}
