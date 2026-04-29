3eb3acffcfb182e1d27093d1bd47340bbe2b8b1a
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2026-04-29 08:04:00 +0100

    [121] [Time Beats: 39.16%] [Memory Beats: 15.41%] - LeetPush

--- Best-Time-to-Buy-and-Sell-Stock.java
+++ Best-Time-to-Buy-and-Sell-Stock.java
@@ -1,22 +1,17 @@
-class Solution {
-    public int maxProfit(int[] prices) {
-        
-        int min = prices[0];
-        int res = 0;
-        int n = prices.length;
-
-        for(int i = 1; i < n ; i++) {
-
-            if(prices[i] < min) {
-                min = prices[i];
-            }
-
-            if(prices[i] - min > res) {
-                res = prices[i] - min;
-            }
-            
-        }
-
-        return res;
-    }
-}
+1class Solution {
+2    public int maxProfit(int[] prices) {
+3        
+4        int maximum = 0;
+5        int min = prices[0];
+6        int n = prices.length;
+7
+8        for(int i = 1; i < n; i++) {
+9            
+10            min = Math.min(min, prices[i]);
+11            maximum = Math.max(maximum, prices[i] - min);
+12        }
+13
+14        return maximum;
+15        
+16    }
+17}
