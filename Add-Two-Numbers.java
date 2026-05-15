3a8a6fb7b54d418d3457fb12636e777b96cdb31a
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2026-05-15 16:59:19 +0100

    [2] [Time Beats: 100.00%] [Memory Beats: 56.94%] - LeetPush

--- Add-Two-Numbers.java
+++ Add-Two-Numbers.java
@@ -1,55 +1,57 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        
-        int sum = 0;
-        int remainder = 0;
-
-        ListNode head = null;
-        ListNode temp = head;
- 
-        while(true) {
-            
-            int value = 0;
-            if(l1 != null) {
-                value += l1.val;
-                l1 = l1.next;
-            }
-
-            if(l2 != null) {
-                value += l2.val; 
-                l2 = l2.next;
-            }
-
-            value += remainder;
-            sum = value % 10;
-
-            if(head == null) {
-                head = new ListNode(sum);
-                temp = head;
-            } else {
-                temp.next = new ListNode(sum);
-                temp = temp.next;
-            }
-            
-            remainder = value / 10;
-
-            if(l1 == null && l2 == null) {
-                if(remainder == 0) {
-                    break;
-                }
-            }
-        }
-
-        return head;
-    }
-}
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11
+12class Solution {
+13    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+14        
+15        ListNode head = null;
+16        ListNode temp = null;
+17
+18        int sum = 0;
+19        int remainder = 0;
+20
+21        while(true) {
+22
+23            int value = 0;
+24            if(l1 != null) {
+25                value += l1.val;
+26                l1 = l1.next;
+27            }
+28
+29            if(l2 != null) {
+30                value += l2.val;
+31                l2 = l2.next;
+32            }
+33
+34            value += remainder;
+35            sum = value % 10;
+36
+37            if(head == null) {
+38                head = new ListNode(sum);
+39                temp = head;
+40            } else {
+41                temp.next = new ListNode(sum);
+42                temp = temp.next;
+43            }
+44            
+45            remainder = value / 10;
+46
+47            if(l1 == null && l2 == null) {
+48                if(remainder == 0) {
+49                    break;
+50                }
+51            }
+52        }
+53
+54        return head;
+55    }
+56}
+57
