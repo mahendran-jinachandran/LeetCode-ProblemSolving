50366d2ae3bc70464be0a8e8cdded4ec2ec3d91d
mahendran-jinachandran <68937987+mahendran-jinachandran@users.noreply.github.com>
2026-05-16 17:11:33 +0100

    [287] [Time Beats: 100.00%] [Memory Beats: 22.84%] - LeetPush

--- Find-the-Duplicate-Number.java
+++ Find-the-Duplicate-Number.java
@@ -1,15 +1,16 @@
-class Solution {
-    public int findDuplicate(int[] nums) {
-        
-        boolean[] numbers = new boolean[nums.length + 1]; 
-        for(int n: nums) {
-            if(numbers[n] == true) {
-                return n;
-            }
-
-            numbers[n] = true;
-        }
-
-        return -1;
-    }
-}
+1class Solution {
+2    public int findDuplicate(int[] nums) {
+3        
+4        boolean[] result = new boolean[nums.length + 1];
+5
+6        for(int n: nums) {
+7            if(result[n] == true) {
+8                return n;
+9            }
+10
+11            result[n] = true;
+12        }
+13
+14        return -1;
+15    }
+16}
